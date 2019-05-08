9c026e91a4f9903d900683b8ac4e5fc7f6423aa1
0x13f <twarmerdam14@gmail.com>
2019-05-08 10:26:29 +0200

    God added

---
+++ src/vergetenstad/player/Remco.java
@@ -0,0 +1,14 @@
+package vergetenstad.player;
+
+import javafx.scene.paint.Color;
+
+public class Remco extends Player {
+	
+	/////////////////////////////////////// Constructor ///////////////////////////////////////
+	
+	public Remco( String nickname ) {
+		
+		super( nickname, "Remco", "PLACEHOLDER", 4, Color.PINK, "resources/placeholder.png" );
+		
+	}
+}
