0a33b1146f0f4c35608b72be65b7976a36bcc020
Ryan Rooijakkers <R.Rooijakkers@student.tudelft.nl>
2019-06-05 12:03:17 +0200

    Storm en StromEvents

---
+++ src/main/java/Model/storm/Storm.java
@@ -0,0 +1,15 @@
+package Model.storm;
+
+public class Storm {
+
+    private int x;
+    private int y;
+    private int sterkte;
+    private StormEvent[] stormEvents = new StormEvent[31];
+
+    public void makeEvents(){
+
+    }
+
+
+}
