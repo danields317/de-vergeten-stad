0ab8658e6d3337018e58af50ccfa124c7f1d1ea1
Daniel <danields317@gmail.com>
2019-06-07 15:54:31 +0200

    Bord helemaal super goed gemaakt

--- src/main/java/View/SpeelbordView.java
+++ src/main/java/View/SpeelbordView.java
@@ -17,16 +17,19 @@ public class SpeelbordView {
 
     public GridPane loadSpelBord(){
         spelbord = new GridPane();
+        spelbord.setLayoutX(410);
+        spelbord.setLayoutY(75);
         tileController = TileController.getInstance();
         Tile[][] tiles = tileController.getTiles();
         for(int i = 0; i < tiles.length; i ++){
             for(int j = 0; j < tiles[i].length; j ++){
-                ImageView tile = new ImageView(tiles[i][j].getImage());
-                tile.setFitHeight(115);
-                tile.setFitWidth(115);
-                spelbord.add(tile, i , j);
-                spelbord.setLayoutX(410);
-                spelbord.setLayoutY(75);
+
+                TileView tileView = new TileView(tiles[i][j].getImage());
+
+                //ImageView tile = new ImageView(tiles[i][j].getImage());
+                //tile.setFitHeight(115);
+                //tile.setFitWidth(115);
+                spelbord.add(tileView.maakTileImage(), i , j);;
                 System.out.println(i + " " + j);
             }
         }
--- src/main/java/View/TileView.java
+++ src/main/java/View/TileView.java
@@ -1,19 +1,34 @@
 package View;
 
 import Controller.Tile_Controllers.TileController;
+import Model.Tiles.Tile;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import observers.BordObservable;
 import observers.BordObserver;
 
 public class TileView implements BordObserver{
 
     TileController tileController;
+    ImageView imageView;
 
-    public TileView(){
+    public TileView(Image image){
         tileController = TileController.getInstance();
         tileController.registerObserver(this);
+        imageView = new ImageView(image);
+        imageView.setFitHeight(115);
+        imageView.setFitWidth(115);
+    }
+
+    public ImageView maakTileImage() {
+
+        return imageView;
+
     }
 
     public void update(BordObservable bo){
 
+        Tile tile = (Tile) bo;
+        imageView.setImage(tile.getImage());
     }
 }
