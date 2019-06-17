a1e8601008a7856c07c087e93c562590279d772a
0x13f <twarmerdam14@gmail.com>
2019-06-17 13:24:18 +0200

    kleinere onderdeel afbeeldingen

--- src/main/java/View/bord_views/EquipmentView.java
+++ src/main/java/View/bord_views/EquipmentView.java
@@ -52,8 +52,8 @@ public class EquipmentView implements PlayerObserver {
                     break;
             }
 
-            ImageView imageView = new ImageView(equipment.getImage());
-            hbox.getChildren().add(imageView);
+            //ImageView imageView = new ImageView(equipment.getImage());
+            //hbox.getChildren().add(imageView);
         }
 
         return hbox;
