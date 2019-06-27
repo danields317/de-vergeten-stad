b0e4da6cfdaeb7d2acb3f0d0eb8286012539b606
0x13f <twarmerdam14@gmail.com>
2019-06-27 14:12:19 +0200

    zonneschild mischie???

--- src/main/java/Controller/Tile_Controllers/TileController.java
+++ src/main/java/Controller/Tile_Controllers/TileController.java
@@ -386,7 +386,7 @@ public class TileController {
             if((tileFB.get("discovered").toString()).equals("true")){
                 discovered = true;
             }
-            boolean hasZonneSchild = Boolean.getBoolean(tileFB.get("hasZonneSchild").toString());
+            boolean hasZonneSchild = tileFB.get("hasZonneSchild").equals("true");
             int aantalZand = Integer.valueOf(tileFB.get("aantalZandTegels").toString());
             tile.emptyPlayers();
             String s = tileFB.get("Players").toString();
