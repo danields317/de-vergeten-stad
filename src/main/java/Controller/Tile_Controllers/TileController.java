d980c71946bb66007af1521b9b15996cd265f94c
Ryan Rooijakkers <R.Rooijakkers@student.tudelft.nl>
2019-06-27 10:57:05 +0200

    bug fixing

--- src/main/java/Controller/Tile_Controllers/TileController.java
+++ src/main/java/Controller/Tile_Controllers/TileController.java
@@ -281,8 +281,9 @@ public class TileController {
         for (Onderdeel onderdeel : onderdelen){
             if(!(onderdeel.getY() == -1) && !(onderdeel.getX() == -1)) {
                 Tile onderdeelSpawn = getTileByLocation(onderdeel.getY(), onderdeel.getX());
-                onderdeelSpawn.removeOnderdeelSoort(onderdeel);
-                onderdeelSpawn.setOnderdeel(onderdeel);
+                if (!onderdeelSpawn.getOnderdelen().contains(onderdeel)){
+                    onderdeelSpawn.setOnderdeel(onderdeel);
+                }
             }
         }
     }
